ddfcb29569464ca2795550e771bdae60d78db2a6
Devdatt17 <devdattpandit99@gmail.com>
2022-01-13 23:10:32 +0530

    simple form using react hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [formValue, setFormValue] = useState({
+    name: "",
+    email: ""
+  })
+
+  const handleName = (event) => {
+    setFormValue(values => ({
+      ...values,
+      name: event.target.value
+    }))
+  }
+
+  const handleEmail = (event) => {
+    setFormValue(values => ({
+      ...values,
+      email: event.target.value
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='Name'
+          value={formValue.name}
+          onChange={handleName}
+        />
+
+        <input
+          type="text"
+          placeholder='Email'
+          value={formValue.email}
+          onChange={handleEmail}
+        />
+      </form>
+      <h2>{formValue.name}</h2>
+      <h3>{formValue.email}</h3>
     </div>
   );
 }
